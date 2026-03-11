cbbb561a67edcbad2dbb532278ba6a4633caac8d
RafiiArifNugroho <rafialamcitra@gmail.com>
2026-03-11 18:22:00 +0700

    pertemuan 4

---
+++ Praktikum 4 PDW/scripts.js
@@ -0,0 +1,16 @@
+// Date() untuk menampilkan tanggal saat ini
+Date();
+
+// Mendapatkan tanggal hari ini
+let tanggal = new Date();
+document.getElementById("tanggal").innerHTML = tanggal;
+
+// untuk menampilkan alert tampil()
+function tampil() {
+    alert("Ini adalah button");
+}
+
+// Untuk menampilkan alert show()
+function show() {
+    alert("ini adalah pop up untuk tulisan perkenalan");
+}
